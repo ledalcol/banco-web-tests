5c8a221fef239fd616b8a5bfae400b1154cd9a38
Leticia <le-dalcol@hotmail.com>
2025-09-11 16:03:33 -0300

    Adicionado screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,6 +3,7 @@ describe('Login', () => {
 
     //Acesso ao site
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-pagina')
   })
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
@@ -10,7 +11,9 @@ describe('Login', () => {
     //Login
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('apos-preencher-dados-validos')
     cy.get('#login-section > .btn').click()
+    cy.screenshot('apos-clicar-no-botao-entrar')
 
     //Verificação
     cy.contains('h4','Realizar Transferência').should('be.visible')
